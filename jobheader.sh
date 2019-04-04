157ba4457b9ca428f0ea4874d5d130ed6bcab896
bcmcpher <bcmcpher@iu.edu>
2019-04-04 13:00:14 -0400

    fixed variable switch for number of requested streamlines

--- jobheader.sh
+++ jobheader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##figure out amount of wall time we really need by parsing config.json with jq
-if [ "$(jq .num_fibers -r config.json)" > 35000 ]; then
+if [ "$(jq .num_fibers -r config.json)" -gt 30000 ]; then
     walltime=12:00:00
 else
     walltime=06:00:00
