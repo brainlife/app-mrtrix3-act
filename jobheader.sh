27806a84e0efa56d236ae591fcc34b1c420a68a1
bcmcpher <bcmcpher@iu.edu>
2019-04-07 15:37:47 -0400

    upped job time for excessive streamline count

--- jobheader.sh
+++ jobheader.sh
@@ -2,7 +2,7 @@
 
 ##figure out amount of wall time we really need by parsing config.json with jq
 if [ "$(jq .num_fibers -r config.json)" -gt 30000 ]; then
-    walltime=12:00:00
+    walltime=36:00:00
 else
     walltime=06:00:00
 fi
